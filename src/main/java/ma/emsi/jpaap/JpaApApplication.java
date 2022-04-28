2aab99a2dc3516306013ffa17a22ccf66e6ba688
Aminelea12 <104215523+Aminelea12@users.noreply.github.com>
2022-04-28 12:58:39 +0000

    Update JpaApApplication.java

--- src/main/java/ma/emsi/jpaap/JpaApApplication.java
+++ src/main/java/ma/emsi/jpaap/JpaApApplication.java
@@ -21,18 +21,22 @@ public class JpaApApplication {
     CommandLineRunner commandLineRunner(PatientRepository patientRepository){
         return args -> {
 
-            patientRepository.save(new Patient(null,"Hassan",new Date(),false,22));
-            patientRepository.save(new Patient(null,"Mouad",new Date(),false,22));
-            patientRepository.save(new Patient(null,"Nassim",new Date(),false,22));
-            patientRepository.save(new Patient(null,"Nada",new Date(),false,22));
-            patientRepository.save(new Patient(null,"Yassine",new Date(),false,22));
-            patientRepository.save(new Patient(null,"Hmed",new Date(),false,22));
-            patientRepository.save(new Patient(null,"Zaid",new Date(),false,22));
-            patientRepository.save(new Patient(null,"Zineb",new Date(),false,22));
+            patientRepository.save(new Patient(null,"amine",new Date(),false,22));
+            patientRepository.save(new Patient(null,"mohammed",new Date(),false,22));
+            patientRepository.save(new Patient(null,"anisa",new Date(),false,22));
+            patientRepository.save(new Patient(null,"hajar",new Date(),false,22));
+            patientRepository.save(new Patient(null,"mehdi",new Date(),false,22));
+            patientRepository.save(new Patient(null,"nizar",new Date(),false,22));
+            patientRepository.save(new Patient(null,"abdo",new Date(),false,22));
+            patientRepository.save(new Patient(null,"younes",new Date(),false,22));
             patientRepository.save(new Patient(null,"Omar",new Date(),false,22));
-            patientRepository.save(new Patient(null,"Youssef",new Date(),false,22));
-            patientRepository.save(new Patient(null,"YYY",new Date(),false,22));
-            patientRepository.save(new Patient(null,"DDDD",new Date(),false,22));
+            patientRepository.save(new Patient(null,"imane",new Date(),false,22));
+            patientRepository.save(new Patient(null,"mahmoud",new Date(),false,22));
+            patientRepository.save(new Patient(null,"lamiae",new Date(),false,22));
+            patientRepository.save(new Patient(null,"hicham",new Date(),false,22));
+            patientRepository.save(new Patient(null,"othmane",new Date(),false,22));
+            patientRepository.save(new Patient(null,"ghita",new Date(),false,22));
+            patientRepository.save(new Patient(null,"rachid",new Date(),false,22));
             patientRepository.findAll().forEach(p -> {
                 System.out.println(p.getNom());
             });
